a352927fb9993b063667e5fda837a65b952b6f06
Jerome Avecilla <jeromesavc@gmail.com>
2024-09-07 16:19:30 +0800

    rebuild porfolio using vuejs

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,42 @@
+<script setup>
+</script>
+
+<template>
+    <header id="header" class="header d-flex align-items-center fixed-top">
+        <div class="container-fluid container-xl position-relative d-flex align-items-center">
+        <a href="#" class="logo d-flex align-items-center me-auto">
+            <img src="@/assets/img/logo.png" alt="logo" loading="lazy" width="30" height="30"/>
+            <h1 class="sitename">JA.</h1>
+        </a>
+
+        <nav id="navmenu" class="navmenu">
+            <ul>
+            <li><a href="#" class="active">Home</a></li>
+            <li><a href="#">About Me</a></li>
+            <li><a href="#">Projects</a></li>
+            <li class="dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                <ul>
+                <li><a href="#">Dropdown 1</a></li>
+                <li><a href="#">Dropdown 2</a></li>
+                <li class="dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down toggle-dropdown"></i></a>
+                    <ul>
+                    <li><a href="#">Deep Dropdown 1</a></li>
+                    <li><a href="#">Deep Dropdown 2</a></li>
+                    <li><a href="#">Deep Dropdown 3</a></li>
+                    <li><a href="#">Deep Dropdown 4</a></li>
+                    <li><a href="#">Deep Dropdown 5</a></li>
+                    </ul>
+                </li>
+                </ul>
+            </li>
+            <li><a href="#">Contact</a></li>
+            </ul>
+            <i class="mobile-nav-toggle d-xl-none bi bi-list"></i>
+        </nav>
+        <a class="btn-get-in-touch" href="#">Get in Touch</a>
+        </div>
+  </header>
+</template>
+
+<style scoped>
+</style>
